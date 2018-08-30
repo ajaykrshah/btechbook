df40b225fa5936ba977ec1a3e768fc491f555201
Ajay Kumar Shah <42646728+ajaykrshah@users.noreply.github.com>
2018-08-30 19:35:22 +0530

    Initial uploading all files
    Uploading a website that having books for b.tech.

---
+++ mail_handler1.php
@@ -0,0 +1,31 @@
+<?php
+	if(isset($_POST['submit'])){
+		$email=$_POST['email'];
+		$msg=$_POST['msg'];
+
+		$to='onlinestudymaterial94@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Website Suggestions ';
+		$headers="From: ".$email;
+        $headers .= "Reply-To:\r\n".$to;
+        $headers .= "Return-Path: onlinestudymaterial94@gmail\n".$to;
+        $headers .= "CC: ".$email;
+        $headers .= "BCC:".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.",ThankYou for Visiting</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+		$headers="From: ".$to;
+		$to=$email;
+		if(mail($to, 'feedback response', 'thanks for your valuable feedback we will surely try our best to end your problems ', $headers)){
+			
+		}
+		else{
+			echo "Something went wrong!";
+		}
+		$headers='';
+         $to='';		
+	}
+?>
